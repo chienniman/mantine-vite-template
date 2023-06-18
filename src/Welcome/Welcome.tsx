39bbf686c49183c0e4c8ae307639e797a7574aa1
齊尼曼 <97031067+chienniman@users.noreply.github.com>
2023-06-18 23:12:06 +0800

    title3

--- src/Welcome/Welcome.tsx
+++ src/Welcome/Welcome.tsx
@@ -6,7 +6,7 @@ export function Welcome() {
       <Text size="xl" weight={500}>
         Welcome to Mantine!
       </Text>
-      <Button>Github Action</Button>
+      <Button>My Github Action</Button>
     </Stack>
   );
 }
